c36547674585930399a0fd9a7046ac2e4fa55e12
playHing <wyhing548@gmail.com>
2019-09-01 22:08:59 +0800

    15r

--- 15.3-sum.java
+++ 15.3-sum.java
@@ -7,41 +7,19 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         if (nums == null || nums.length < 3) return new LinkedList<>();
         List<List<Integer>> res = new LinkedList<>();
-        Set<Item> seemRes = new HashSet<>();
+        Arrays.sort(nums);
         for (int t = nums.length - 1; t >= 0; --t) {
-            Map<Integer, Integer> idxMap = new HashMap<>();
-            for (int i = 0; i < t; ++i) {
-                if (idxMap.containsKey(nums[i])) {
-                    Item item = new Item(nums[idxMap.get(nums[i])], nums[i], nums[t]);
-                    if (!seemRes.contains(item)) {
-                        res.add(Arrays.asList(new Integer[] {item.a, item.b, item.c}));
-                        seemRes.add(item);
-                    }
-                }
-                else
-                    idxMap.put(-nums[t]-nums[i], i);
+            if (t < nums.length - 1 && nums[t] == nums[t+1]) continue;
+            int i = 0, j = t - 1, vt = nums[t];
+            while (i < j) {
+                int vi = nums[i], vj = nums[j];
+                int sum = vi + vj + vt;
+                if (sum == 0) res.add(Arrays.asList(vi, vj, vt));
+                if (sum <= 0) while (i < j && nums[++i] == vi);
+                if (sum >= 0) while (i < j && nums[--j] == vj);
             }
         }
         return res;
-    }
-
-    class Item {
-        public final int a, b, c;
-        public Item(int x, int y, int z) {
-            int[] tmp = new int[] {x, y, z};
-            Arrays.sort(tmp);
-            a = tmp[0]; b = tmp[1]; c = tmp[2];
-        }
-        
-        @Override
-        public boolean equals(Object oo) {
-            Item o = (Item) oo;
-            return a == o.a && b == o.b && c == o.c;
-        }
-        @Override
-        public int hashCode() {
-            return a * 11 * 11 + b * 7 + c * 3;
-        }
     }
 }
 
